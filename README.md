fd5e08ce8b2f2cfc0caa2a54b2f4ceb10fb2be72
David Pitt <dpitt00@gmail.com>
2014-12-21 11:40:39 -0800

    Maybe this will be the only commit I need :D

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
+### THIS IS BY DAVID PITT HOORAY I'LL LEAVE THE REST OF THIS THE SAME
+
 ### Introduction
+
 
 This second programming assignment will require you to write an R
 function that is able to cache potentially time-consuming computations.
--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+## == chachematrix.R ==
+## -- By: David Pitt (github.com/dpitty)
 
-## Write a short comment describing this function
+## makeCacheMatrix() is a custom implementation of the matrix class
+## which allows chaching the matrix's inverse (aka "solve()") after one
+## computation, such that future calls to cacheSolve() retrieve the
+## inverse value instantly, instead of requiring costly repeated
+## calls to solve().
+## NOTE: the program only works on invertible matrices and will throw an
+## error on cacheSolve() if the input matrix is not invertible.
+
+## Function makeCacheMatrix(x = matrix()) converts a standard matrix 
+## into a cahce-able one, represented as a list of functions which are
+## used in cacheSolve() (not intended for the programmer's direct use).
+## -- Usage: --
+## myNormalMatrix <- matrix(nrow = 2, ncol = 2, 1:4)
+## myCacheMatrix <- makeCacheMatrix(myNormalMatrix)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  savedInverse <- NULL
+  setmatrix <- function(y) {
+    x <<- y
+    savedInverse <<- NULL
+  }
+  getmatrix <- function() { 
+    x
+  }
+  setinverse <- function(initialInverse) {
+    savedInverse <<- initialInverse
+  }
+  getinverse <- function() {
+    savedInverse
+  }
+  list(setmatrix = setmatrix, getmatrix = getmatrix,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## Function cacheSolve(x, ...) works the same as solve() on an ordinary
+## matrix, but requires a chacheMatrix as its input. Returns the inverse
+## of the original matrix.
+## -- Usage: --
+## inverse <- cacheSolve(myCacheMatrix)
+## cacheSolve() allows passing multiple parameters with the same rules
+## as the solve() function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  cachedInverse <- x$getinverse()
+  if (!is.null(cachedInverse)) {
+    message("getting cached inverse")
+    return(cachedInverse)
+  } else {
+    originalMatrix <- x$getmatrix()
+    inverse <- solve(originalMatrix, ...)
+    x$setinverse(inverse)
+    inverse
+  }
 }
